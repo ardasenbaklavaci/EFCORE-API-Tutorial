85b612d6702f4882b27ed6182994745340f68656
ardasenbaklavaci <107650690+ardasenbaklavaci@users.noreply.github.com>
2024-06-27 10:48:16 +0300

    Init
    Initial Commit

---
+++ WebApplication6/Controllers/BookController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication6.Models;
+
+namespace WebApplication6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly apiProjContext _context;
+
+        // Constructor with the required DbContext parameter
+        public BookController(apiProjContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Book
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+        {
+            return await _context.Books.ToListAsync();
+        }
+
+        // GET: api/Book/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(int id)
+        {
+            var book = await _context.Books
+                                     .Where(b => b.Id == id)
+                                     .FirstOrDefaultAsync();
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        
+
+        
+    }
+}
